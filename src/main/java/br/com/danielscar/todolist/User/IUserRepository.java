adca80035c6f277527b815c17bafcc8e6173ac56
Daniel Oliveira Scarlati <176938524+danielscarlati@users.noreply.github.com>
2025-08-01 09:37:50 -0300

    API do Curso de Spring Boot Introdutorio da Rocketseat

---
+++ src/main/java/br/com/danielscar/todolist/User/IUserRepository.java
@@ -0,0 +1,12 @@
+package br.com.danielscar.todolist.User;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findbyUsername(String username);
+
+}
